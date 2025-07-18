55e55b4a79d9f4ee03bf5b4c390fa1ed938e28dc
RUTENCO <daniel.ramirez7@udea.edu.co>
2025-07-18 14:42:50 -0500

    Ignore .env file containing credentials

--- build/server.js
+++ build/server.js
@@ -1,8 +1,5 @@
 /* eslint-disable no-console */
 require('dotenv').config();
-console.log('► MAIL_USER=', process.env.MAIL_USER);
-console.log('► MAIL_PASS=', process.env.MAIL_PASS);
-
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
