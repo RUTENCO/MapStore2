4fcaaeeb2b715ba5164e51352938206ef5e94ad9
RUTENCO <daniel.ramirez7@udea.edu.co>
2025-07-18 13:46:55 -0500

    Ignore .env file containing credentials

--- build/server.js
+++ build/server.js
@@ -41,7 +41,7 @@ app.post('/send-email', (req, res) => {
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
